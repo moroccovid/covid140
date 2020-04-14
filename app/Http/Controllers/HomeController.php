86bf8d383171a41498d5d4b3f695c0b8be87d83f
macbookair <lotfi1002@gmail.com>
2020-04-14 22:27:50 +0100

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fiche;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,13 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+
+        $fiches = Fiche::Where('traite', 0)->orderBy('created_at','DESC')->paginate(5);
+        return view('home',compact('fiches'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
+
+
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,47 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Les fiches non traitees</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <tr>
+                    <th>No</th>
+                    <th>Nom</th>
+                    <th>Email</th>
+                    <th>Telephone</th>
 
-                    You are logged in!
+                    </tr>
+                @if(!empty($fiches) && $fiches->count())
+                        @foreach ($fiches as $key => $fiche)
+                        <tr>
+                            <td>{{ ++$i }}</td>
+                            <td>{{ $fiche->nom }}</td>
+                            <td>{{ $fiche->email }}</td>
+
+                            <td>{{ $fiche->telephone }}</td>
+
+
+                        </tr>
+                        @endforeach
+                        @else
+                                <tr>
+                                    <td colspan="10"> Tableau vide.</td>
+                                </tr>
+                    @endif
+                </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Le graphe de la contamination</div>
+
+                <div class="card-body">
+
                 </div>
             </div>
         </div>
